be1d8809faf1b65a8efa3a78efd5ae900a525999
Kurt Schwind <kurt.schwind@gmail.com>
2013-01-06 19:52:38 -0600

    Removing references to svn
    No need to have references for SVN

--- production_push.sh
+++ production_push.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 
-cd ..
-cd 70s_release
-# svn --username kurtschwind export http://70sdetective.com/edetect 70s_release
 scp -r * kurtschwind@70sdetective.com:70sdetective.com/ 
 ssh kurtschwind@70sdetective.com "cd 70sdetective.com;unzip resources/support/ext*zip"
 echo "Application Deployed"
