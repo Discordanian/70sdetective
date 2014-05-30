00746cb91752dabfee14f36d2b72913d42a2f940
Discordanian <kurt.schwind@gmail.com>
2014-05-30 10:53:25 -0500

    Yeoman
    Yeow.  Man.

---
+++ detective/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('detectiveApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
