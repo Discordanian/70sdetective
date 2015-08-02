32473189c16bd3b995a44886b8a4690178b514bb
Kurt Schwind <Kurt.schwind@gmail.com>
2015-08-01 20:54:16 -0500

    New push

--- push.sh
+++ push.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+    scp -rv . kurtschwind@speyburn.dreamhost.com:/home/kurtschwind/70sdetective.com/
+exit
 
 if [ -f /Users/kurtschwind/src/webster/cosc5030/CardTable/push.sh ]
 then
 	echo "Mac Laptop Directory"
-	cd /Users/kurtschwind/src/webster/cosc5030/CardTable
+	cd /Users/kurtschwind/src/70sdetective/
 fi
 
 if [ -f /home/n620911/src/70sdetective/push.sh ]
@@ -19,5 +21,5 @@ then
 	echo "At work, use KURT web directory"
 else
 	echo "Not at work.  SCP this to DreamHost"
-	# scp -rv . kurtschwind@speyburn.dreamhost.com:/home/kurtschwind/slithytoves.org/webster/
+	scp -rv . kurtschwind@speyburn.dreamhost.com:/home/kurtschwind/70sdetective.com/
 fi
