922d9c6d0433e44ce2604d91e2978d29614ac4a1
Kurt Schwind <Kurt.Schwind@gmail.com>
2015-08-03 08:24:57 -0500

    Push just stonesun

--- stonesun/push.sh
+++ stonesun/push.sh
@@ -1,22 +1,4 @@
 #!/bin/sh
 
+scp -rv . kurtschwind@speyburn.dreamhost.com:/home/kurtschwind/70sdetective.com/stonesun/
 
-if [ -f /Users/kurtschwind ]
-then
-	echo "Mac Laptop Directory"
-fi
-
-if [ -f /home/n620911 ]
-then
-	echo "TDCSBATCHLAB detected"
-	cd /home/n620911/src/70sdetective
-fi
-
-if [ "`hostname`" == "tdcsbatchlab" ]
-then
-	cp -rv . /var/www/html/KURT/
-	echo "At work, use KURT web directory"
-else
-	echo "Not at work.  SCP this to DreamHost"
-	# scp -rv . kurtschwind@speyburn.dreamhost.com:/home/kurtschwind/slithytoves.org/webster/
-fi
