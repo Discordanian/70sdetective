f756fcb378061068f63e529d92da1f8766b50682
Kurt Schwind <Kurt.Schwind@gmail.com>
2015-08-03 13:08:54 -0500

    Band title and impairment

--- stonesun/app/GameManager.js
+++ stonesun/app/GameManager.js
@@ -1,5 +1,16 @@
 var GameManager = function () {
         var first = true;
+        var impaired = false;
+
+        var impairl = function(b) {
+            WGO.impair(!!b);
+            WGO.refresh();
+            Grapevine.impair(!!b);
+            Grapevine.refresh();
+            Band.impair(!!b);
+            Band.refresh();
+            return impaired;
+        };
 
         var restart = function() {
             Band.clear();
@@ -14,13 +25,13 @@ var GameManager = function () {
                 $("#new_game").click(function()   { restart(); });
                 $("#tours").click(function()      { Grapevine.otherSong(); Grapevine.refresh(); });
                 $("#charts").click(function()     { Grapevine.addItem("You are on the charts"); Grapevine.refresh(); });
-                $("#how_to_play").click(function(){ Grapevine.addItem("You are on the charts"); Grapevine.refresh(); });
+                $("#how_to_play").click(function(){ impaired = !impaired; impairl(impaired); });
                 $("#releases").click(function()   { WGO.addItem("Releases selected"); WGO.refresh(); });
                 $("#drugs").click(function()      { Band.drugoffer("lsd",false); });
             }
 
             return true;
-        }
+        };
 
     // Return public interface
     return {
@@ -31,10 +42,7 @@ var GameManager = function () {
             return restart();
         },
         impair: function(b) {
-            WGO.impair(!!b);
-            WGO.refresh();
-            Grapevine.impair(!!b);
-            Grapevine.refresh();
+            return impairl(b);
         }
     }; // end return of public object
 
